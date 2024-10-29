6cab0f1ee94410af6f09ec3c9e605faeddd7b7d1
Uladzislau <3xecvte@gmail.com>
2024-10-29 17:53:21 +0200

    1/q/1: done

--- lab1/Q1/main.py
+++ lab1/Q1/main.py
@@ -16,6 +16,8 @@ class FrontMiddleBackQueue:
 
     def __getLastNode(self) -> Node:
         nodeStep = self.head
+        if self.size == 1:
+            return nodeStep
         for _ in range(self.size - 1):
             nodeStep = nodeStep.next
         return nodeStep
@@ -49,7 +51,8 @@ class FrontMiddleBackQueue:
             self.head = newNode
         else:
             nodeStep = self.head
-            for _ in range(ceil(self.size / 2)):
+            tempRange = ceil(self.size / 2)
+            for _ in range(tempRange):
                 nodeStep = nodeStep.next
 
             if nodeStep is None:
@@ -59,7 +62,7 @@ class FrontMiddleBackQueue:
             newNode.prev = nodeStep.prev
             nodeStep.prev.next = newNode
             nodeStep.prev = newNode
-                
+
         self.size += 1
 
     def pushBack(self, val: int) -> None:
@@ -76,6 +79,11 @@ class FrontMiddleBackQueue:
     def popFront(self) -> int:
         if self.size == 0:
             return -1
+        elif self.size == 1:
+            answer = self.head.val
+            self.head = None
+            self.size -= 1
+            return answer
         else:
             lastNode = self.__getLastNode()
             if lastNode is None or lastNode.prev is None:
@@ -119,23 +127,36 @@ class FrontMiddleBackQueue:
 # if __name__ == "__main__":
 #     methods = ["FrontMiddleBackQueue","popMiddle","popMiddle","pushMiddle","pushMiddle","popMiddle","popMiddle","popMiddle","popBack","popMiddle","popFront","pushBack","popFront","pushMiddle","pushMiddle","popMiddle","popBack","pushFront","popMiddle","pushMiddle","pushMiddle","pushMiddle","popMiddle","pushMiddle","popBack","pushMiddle","popMiddle","popMiddle","popMiddle","popMiddle","popFront","pushMiddle","pushMiddle","pushMiddle","pushFront"]
 #     var = [[],[],[],[773222],[279355],[],[],[],[],[],[],[448905],[],[168284],[874541],[],[],[15656],[],[803226],[720129],[626048],[],[860306],[],[630886],[],[],[],[],[],[837735],[414354],[404946],[88719]]
+#     expected = [0,-1,-1,0,0,279355,773222,-1,-1,-1,-1,0,448905,0,0,874541,168284,0,15656,0,0,0,626048,0,803226,0,630886,720129,860306,-1,-1,0,0,0,0]
 #     operations = []
 #     for i in range(len(methods)):
-#         operations.append([methods[i], var[i]])
+#         operations.append([methods[i], var[i], expected[i]])
         
 #     my_linked_list = FrontMiddleBackQueue()
-#     for operation, args in operations:
+#     answer = 0
+#     for operation, args, expected in operations:
 #         print(operation, args)
 #         if operation == "pushFront":
 #             my_linked_list.pushFront(args[0])
+#             answer = 0
 #         elif operation == "pushMiddle":
 #             my_linked_list.pushMiddle(args[0])
+#             answer = 0
 #         elif operation == "pushBack":
 #             my_linked_list.pushBack(args[0])
+#             answer = 0
 #         elif operation == "popFront":
-#             print(my_linked_list.popFront())
+#             answer = my_linked_list.popFront()
+#             print(answer)
 #         elif operation == "popMiddle":
-#             print(my_linked_list.popMiddle())
+#             answer = my_linked_list.popMiddle()
+#             print(answer)
 #         elif operation == "popBack":
-#             print(my_linked_list.popBack())
+#             answer = my_linked_list.popBack()
+#             print(answer)
+
+#         if answer != expected:
+#             print("Wrong Answer!!!!!!!!!")
+#             print("Expected: ", expected)
+#             print("Answer: ", answer)
 #         print(my_linked_list)
