59305b36e0b880c37705b7b518a59b5dc3c686b9
Uladzislau <3xecvte@gmail.com>
2024-10-29 16:33:38 +0200

    1/ll/4: done

--- lab1/LL4/main.py
+++ lab1/LL4/main.py
@@ -5,4 +5,14 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        length: int = self.__length(head)
+        for _ in range(length // 2):
+            head = head.next
+        return head
         
+    def __length(self, head: Optional[ListNode]) -> int:
+        length: int = 0
+        while head is not None:
+            length += 1
+            head = head.next
+        return length
