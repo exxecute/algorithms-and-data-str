f12116338b4328eb8a3d5e45ec36f8910a3c2417
Uladzislau <3xecvte@gmail.com>
2024-10-29 18:03:17 +0200

    1/q/3: done

---
+++ lab1/Q3/main.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.__checkTile(grid, i, j)
+                    count += 1
+
+        for i in range(len(grid)):
+            print(grid[i])
+        return count
+    
+    def __checkTile(self, grid, i, j):
+        if i<0 or j<0 or i>=len(grid) or j>=len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.__checkTile(grid, i+1, j)
+        self.__checkTile(grid, i-1, j)
+        self.__checkTile(grid, i, j+1)
+        self.__checkTile(grid, i, j-1)
+        
+# grid1 = [
+#   ["1","1","0","0","0"],
+#   ["1","1","0","0","0"],
+#   ["0","0","1","0","0"],
+#   ["0","0","0","1","1"]
+# ]
+# print(Solution().numIslands(grid1))
