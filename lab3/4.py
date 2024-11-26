05c1a4c24ef2f72e9421c15c8bf8bf20cf356c85
execute <3xecvte@gmail.com>
2024-11-26 23:53:28 +0200

    3/4: done

--- lab3/4.py
+++ lab3/4.py
@@ -1,5 +1,27 @@
 from typing import List
 
 class Solution:
-    def longestCycle(self, edges: List[int]) -> int:
+    def longestCycle(self, edges: List[int]):
+        cycleLength = [-1] * len(edges) 
+        visited = [False] * len(edges) 
+        maxLength = -1  
         
+        for i in range(len(edges)): 
+            if not visited[i]: 
+                visited[i] = True 
+                destination = edges[i] 
+                length = 1  
+                nodeToDistance = {} 
+                nodeToDistance[i] = 0  
+                
+                while destination != -1 and not visited[destination]:
+                    nodeToDistance[destination] = length  
+                    visited[destination] = True  
+                    destination = edges[destination] 
+                    length += 1 
+                
+                if destination != -1 and destination in nodeToDistance:
+                    cycleLength[destination] = length - nodeToDistance[destination]
+                    maxLength = max(maxLength, length - nodeToDistance[destination])
+        
+        return maxLength
