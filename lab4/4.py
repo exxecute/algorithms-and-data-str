9c1f31abfc04b9f5536bd1685af73eec227c562a
execute <3xecvte@gmail.com>
2024-12-10 20:30:14 +0200

    4/4: done

--- lab4/4.py
+++ lab4/4.py
@@ -1,25 +1,19 @@
 from collections import deque, defaultdict
-from heapq import heappop, heappush
 from typing import List
-
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        # Using modified Dijkstra's algorithm
-        # F + C * log(F) where F is the number of flights and C is the number of cities
-
         graph = defaultdict(list)
         for source, target, cost in flights:
             graph[source].append((target, cost))
 
         queue = [(src, 0, 0)]
 
-        # (node, stops) -> total_cost
-        min_cost = defaultdict()
+        min_cost = defaultdict(lambda: float('inf'))
         min_cost[(src, 0)] = 0
 
         while queue:
-            cost, current_city, stops = heappop(queue)
+            cost, current_city, stops = queue.pop()
 
             if current_city == dst:
                 return cost
@@ -31,6 +25,6 @@ class Solution:
                 new_cost = cost + price
                 if new_cost < min_cost[(neighbor, stops + 1)]:
                     min_cost[(neighbor, stops + 1)] = new_cost
-                    heappush(queue, (new_cost, neighbor, stops + 1))
+                    queue.append((new_cost, neighbor, stops + 1))
 
         return -1
