c5e8b1b4339b4618e589d2fe5bc4ccf74cc9f520
execute <3xecvte@gmail.com>
2024-12-10 20:34:20 +0200

    4/5: copy

--- lab4/5.py
+++ lab4/5.py
@@ -1,3 +1,34 @@
+from collections import defaultdict
+from typing import List
+# Double DFS with memoization
+# O(n)
+
 class Solution:
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
-        pass
+        graph = defaultdict(list)
+        for source, target in edges: # undirected graph
+            graph[source].append(target)
+            graph[target].append(source)
+
+        dist = [0] * n  # sum of distances to all other nodes
+        size = [1] * n  # subtree size including itself
+
+        # Calculate subtree size and sum of distances for each node
+        def dfs1(node, parent):
+            for neighbor in graph[node]:
+                if neighbor != parent:
+                    dfs1(neighbor, node)
+                    size[node] += size[neighbor]
+                    dist[node] += dist[neighbor] + size[neighbor]
+        # Recalculate sum of distances for each node
+        # Take into account the size of the subtree of the current node and it's position in the tree
+        def dfs2(node, parent):
+            for neighbor in graph[node]:
+                if neighbor != parent:
+                    dist[neighbor] = dist[node] - size[neighbor] + (n - size[neighbor])
+                    dfs2(neighbor, node)
+
+        dfs1(0, -1)
+        dfs2(0, -1)
+
+        return dist
