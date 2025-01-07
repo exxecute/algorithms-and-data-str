4e1eb641ce9e0543516f0aa8e4e2b537147377e5
execute <3xecvte@gmail.com>
2025-01-07 14:07:09 +0200

    5/6: done

---
+++ lab5/6.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        updated_board = [[0 for _ in range(len(board[0]))] for _ in range(len(board))]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                
